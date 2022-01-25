786574a3b112f8a7b142539cbab1b538e8d807b5
BetaOrbiter <eto_li@outlook.com>
2022-01-25 21:06:01 +0800

    lab3:Detecting which pages have been accessed

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -80,7 +80,37 @@ sys_sleep(void)
 int
 sys_pgaccess(void)
 {
+  //I'm lazy !!!(理直气壮,手动叉腰)
+  extern pte_t *walk(pagetable_t pagetable, uint64 va, int alloc);
+
   // lab pgtbl: your code here.
+  uint64 va;
+  int npages;
+  uint64 dst;
+  unsigned int bitmask;
+  pagetable_t pagetable;
+
+  if(argaddr(0, &va) < 0)
+    return -1;
+  if(argint(1, &npages) < 0)
+    return -1;
+  if(argaddr(2, &dst) < 0)
+    return -1;
+  bitmask = 0;
+  pagetable = myproc()->pagetable;
+
+  for(int i=0; i<npages && i<(sizeof(bitmask)<<3); ++i){//prevent bitmask overflows
+    pte_t *pte = walk(pagetable, va, 0);
+    if(pte == 0)
+      break;
+    if(*pte & PTE_A){
+      bitmask |= (1U << i);
+      *pte = (*pte ^ PTE_A);
+    }
+    va += PGSIZE;
+  }
+  if(copyout(pagetable, dst, (char*)&bitmask, sizeof(bitmask)) < 0)
+    return -1;
   return 0;
 }
 #endif
